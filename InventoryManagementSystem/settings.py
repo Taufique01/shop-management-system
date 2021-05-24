ab926a1a3295bc38abdeda1a04a4948942dbd068
Taufique01 <taufiquehr@gmail.com>
2021-05-24 22:58:08 +0600

    fix staticfiles error heroku

--- InventoryManagementSystem/settings.py
+++ InventoryManagementSystem/settings.py
@@ -3,7 +3,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -126,13 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, './templates/build'),
-	
-)
 
-STATIC_ROOT=os.path.join(BASE_DIR,"static")
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'templates/build'),
+)
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8000",
