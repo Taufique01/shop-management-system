e20385689c898e101b491dba9cff51b902793c02
Taufique01 <50284905+Taufique01@users.noreply.github.com>
2021-05-25 04:39:04 +0600

    Update settings.py

--- InventoryManagementSystem/settings.py
+++ InventoryManagementSystem/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3zue&@nv@q_*70)!#ntcfxn8k(k8xa#o-q+jzk$l&+xj%)(51r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
